350fc754e3507ac8588b4e6b73f11d8111e6dfe6
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 13:29:12 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,6 +3,7 @@ def levitation_quiz (answer)
       puts 'What is the spell that enacts levitation?'
       answer = gets.chomp
       break if answer == "Wingardium Leviosa"
+    end
   puts "You passed the quiz!"
 end
 
